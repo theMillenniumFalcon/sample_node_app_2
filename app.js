d4d285742fef391c537c84d5adec9c3d7df4e609
theMillenniumFalcon <nishankpr2002@gmail.com>
2021-09-30 17:51:49 +0530

    db_connected

--- app.js
+++ app.js
@@ -1,1 +1,43 @@
-console.log('04 Store API')
+require('dotenv').config()
+// async errors
+
+const express = require('express')
+const app = express()
+const connectDB = require('./db/connect')
+const notFoundMiddleware = require('./middleware/not-found')
+const errorMiddleware = require('./middleware/error-handler')
+const port  =process.env.PORt || 3000
+
+// middleware
+app.use(express.json())
+
+// routes
+app.get('/', (req, res) => {
+    res.send('Sample route')
+})
+
+// product route
+
+
+app.use(notFoundMiddleware)
+app.use(errorMiddleware)
+
+const server = async () => {
+    try {
+        await connectDB(process.env.MONGO_URL)
+        app.listen(port, () => {
+            console.log(`Connected to DB and listening on port ${port}...`)
+        })
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+server()
+
+process.on('uncaughtException', (err, promise) => {
+    console.log(`Logged Error: ${err}`)
+    server.close(() => process.exit(1))
+})
+
+
